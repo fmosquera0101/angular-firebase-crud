8d7845bf98586dbc33dcc482bd87a588b53d56d6
fmosquera0101 <fmosquera0101@gmail.com>
2019-08-12 15:36:39 -0500

    Se agrega Css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import {Observable} from 'rxjs';
+import {Show} from './show.interface';
+import {AngularFirestore, AngularFirestoreCollection} from '@angular/fire/firestore';
+
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
+
 export class AppComponent  {
   name = 'Angular';
+  
 }
+
+
