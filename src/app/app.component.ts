0690bc2da633ef63723c3795a58ee12156753c20
fmosquera0101 <fmosquera0101@gmail.com>
2019-08-12 16:11:45 -0500

    update name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,31 @@ import {AngularFirestore, AngularFirestoreCollection} from '@angular/fire/firest
 
 export class AppComponent  {
   name = 'Angular';
+  showCollection: AngularFirestoreCollection<Show>;
+  showList: Observable<Show[]>;
+  constructor(private db: AngularFirestore){
+    this.showCollection = db.collection('fredy_shows');
+    this.showList = this.showCollection.valueChanges();
+  }
+  add(){
+    const id = this.db.createId();
+    const picture = this.getRandomPicture();
+    const name = '';
+    this.showCollection.doc(id).set({name, picture, id});
+  }
+ getRandomPicture(){
+   const randomNumber = Math.round(Math.random() * 20);
+  return 'https://picsum.photos/100/100?image=' + randomNumber;
+ }
+ updatePicture(id: string){
+   const picture = prompt('Please enter an image');
+   if(picture){
+     this.showCollection.doc(id).update({picture});
+   }
+ }
+ updateName(id: string, name: string){
+   this.showCollection.doc(id).update({name});
+ }
   
 }
 
