d997fe68abbb5e6027d1052db2a2492d3d6d536a
fmosquera0101 <fmosquera0101@gmail.com>
2019-08-12 15:21:59 -0500

    Se agrega firebse  moduel storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,23 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp({
+      apiKey: 'AIzaSyBIM6XVuaqeBDtyyB9Ef1U5oUv9Cue9BK8',
+      authDomain: 'first-firebase-angular.firebaseapp.com',
+      databaseURL: 'https://first-firebase-angular.firebaseio.com',
+      projectId: 'first-firebase-angular',
+      storageBucket: 'first-firebase-angular.appspot.com',
+      messagingSenderId: '306103315077',
+    }),
+    AngularFirestoreModule,
+  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
